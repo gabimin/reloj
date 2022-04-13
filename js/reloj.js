cdc8232c6dde7613674ee1ebbf2874cb601c5583
gabimin <gabimin2030@gmail.com>
2022-04-13 03:47:35 -0300

    first

---
+++ js/reloj.js
@@ -0,0 +1,46 @@
+console.log("reloj");
+const mostrarReloj = () => {
+  let fecha = new Date();
+  let hr = formatoHora(fecha.getHours());
+  let min = formatoHora(fecha.getMinutes());
+  let seg = formatoHora(fecha.getSeconds());
+  document.getElementById("hora").innerHTML = `${hr}:${min}:${seg}`;
+
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  const dias = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  let diaSemana = dias[fecha.getDay()];
+  let dia = fecha.getDate();
+  let mes = meses[fecha.getMonth()];
+  let fechaTexto = `${diaSemana} ${dia} de ${mes}`;
+  document.getElementById("fecha").innerHTML = fechaTexto;
+
+  /* document.getElementById("contenedor").classList.toggle("animar"); */
+};
+
+const formatoHora = (hora) => {
+  if (hora < 10) hora = "0" + hora;
+  return hora;
+};
+
+setInterval(mostrarReloj, 1000);
